de6151d41054210f9bf999476749abfab99aa571
nforn22 <fornarinicolas22@gmail.com>
2025-05-26 23:34:45 +0200

    fix CSS + mapping Jobs

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,73 +1,22 @@
 import Job from './Job';
+import jobsData from '../../data/jobs';
+
 
 function Jobs() {
-  return (
-    <main className="jobs">
-    <Job
-        className="red"
-        title="Full Time Sales Associate - Sydney Boutique"
-        contractType="CDI"
-        country="Australie"
-        city="Sydney"
-    />
-    <Job
-        className="green"
-        title="Agent de Sécurité - Pantin"
-        contractType="CDI"
-        country="France"
-        city="Pantin"
-    />
-    <Job
-        className="yellow"
-        title="Responsable d'Atelier (H/F)"
-        contractType="CDI"
-        country="France"
-        city="Paris"
-    />
-    <Job
-        className="blue"
-        title="Chef de Projets"
-        contractType="CDD"
-        country="France"
-        city="Paris"
-    />
-    <Job
-        className="pink"
-        title="Développeur React.js"
-        contractType="CDI"
-        country="France"
-        city="Paris"
-    />
-    <Job
-        className="red"
-        title="Sales Associate Stockholm"
-        contractType="CDI"
-        country="Suède"
-        city="Stockholm"
-    />
-    <Job
-        className="green"
-        title="Vendeur/se - Crans Montana"
-        contractType="CDI"
-        country="Suisse"
-        city="Crans-Montana"
-    />
-    <Job
-        className="yellow"
-        title="CRM & Data Quality Analyst"
-        contractType="CDI"
-        country="USA"
-        city="New York"
-    />
-    <Job
-        className="blue"
-        title="Infirmier (H/F)"
-        contractType="CDI"
-        country="France"
-        city="Pantin"
-    />
-    </main>
-  );
-}
+    return (
+        <main className="jobs">
+            {jobsData.map(job => (
+                <Job 
+                    key={job.id}
+                    className={job.className}
+                    title={job.title}
+                    contractType={job.contractType}
+                    country={job.country}
+                    city={job.city}
+                />
+            ))}
+        </main>
+    )
+};
 
 export default Jobs;
---
+++ src/data/jobs.jsx
@@ -0,0 +1,76 @@
+const jobsData = [
+    {
+      id: 1,
+      className: "red",
+      title: "Full Time Sales Associate - Sydney Boutique",
+      contractType: "CDI",
+      country: "Australie",
+      city: "Sydney"
+    },
+    {
+      id: 2,
+      className: "green",
+      title: "Agent de Sécurité - Pantin",
+      contractType: "CDI",
+      country: "France",
+      city: "Pantin"
+    },
+    {
+      id: 3,
+      className: "yellow",
+      title: "Responsable d'Atelier (H/F)",
+      contractType: "CDI",
+      country: "France",
+      city: "Paris"
+    },
+    {
+      id: 4,
+      className: "blue",
+      title: "Chef de Projets",
+      contractType: "CDD",
+      country: "France",
+      city: "Paris"
+    },
+    {
+      id: 5,
+      className: "pink",
+      title: "Développeur React.js",
+      contractType: "CDI",
+      country: "France",
+      city: "Paris"
+    },
+    {
+      id: 6,
+      className: "red",
+      title: "Sales Associate Stockholm",
+      contractType: "CDI",
+      country: "Suède",
+      city: "Stockholm"
+    },
+    {
+      id: 7,
+      className: "green",
+      title: "Vendeur/se - Crans Montana",
+      contractType: "CDI",
+      country: "Suisse",
+      city: "Crans-Montana"
+    },
+    {
+      id: 8,
+      className: "yellow",
+      title: "CRM & Data Quality Analyst",
+      contractType: "CDI",
+      country: "USA",
+      city: "New York"
+    },
+    {
+      id: 9,
+      className: "blue",
+      title: "Infirmier (H/F)",
+      contractType: "CDI",
+      country: "France",
+      city: "Pantin"
+    }
+];
+  
+export default jobsData;
